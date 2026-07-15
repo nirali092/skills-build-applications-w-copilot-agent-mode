3aa6a918bf8705d1b1cf88e87de3cb1cd2f50534
nirali092 <nirali092@gmail.com>
2026-07-15 14:47:39 +0000

    feat: restructure server initialization by separating server logic into server.js

--- octofit-tracker/backend/dist/index.js
+++ octofit-tracker/backend/dist/index.js
@@ -1,20 +1,3 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const cors_1 = __importDefault(require("cors"));
-const dotenv_1 = __importDefault(require("dotenv"));
-const express_1 = __importDefault(require("express"));
-const database_1 = require("./config/database");
-const routes_1 = __importDefault(require("./routes"));
-dotenv_1.default.config();
-const app = (0, express_1.default)();
-const port = Number(process.env.PORT || 8000);
-app.use((0, cors_1.default)());
-app.use(express_1.default.json());
-app.use(routes_1.default);
-void (0, database_1.connectDatabase)();
-app.listen(port, () => {
-    console.log(`OctoFit Tracker backend listening on port ${port}`);
-});
+require("./server");
